3f83b9c3b30cf6be95be13504b9292721da27c64
dannygallant <danny.gallant@att.net>
2015-07-07 19:02:06 -0500

    Input control logic completed
    Added function to check that user input is a whole number, between 20 &
    1000, and provide feedback.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,27 @@
 $(document).ready(function(){
 
-var maxValue = parseInt(prompt('Enter a number between 20 and 100 for the upper limit of FizzBuzz'));
+var maxValue = prompt('Enter a number between 20 and 1000 for the upper limit of FizzBuzz');
 
-if (maxValue !== parseInt(maxValue)) {
-	alert("FizzBuzz can only accept numerical values. Please re-enter an integer.");
+testInput(maxValue);
+
+console.log(maxValue);  // Used for testing, but left active. Not necessary.
+
+function testInput() {
+	maxValue = +maxValue;	 // Convert user entry into a number.
+	if (Number.isNaN(maxValue)) {
+		alert("FizzBuzz can only accept numerical values. Please reload the page and enter an integer.");
+	} else if (maxValue > 1000) {
+		alert("No numbers over 1000, or you'll be waiting a while.");
+	} else if (maxValue < 20) {
+		alert("Please enter a number between 20 and 1000 to get the full FizzBuzz effect.");
+	} else if (Number.isInteger(maxValue)) {
+		alert("Thanks for the integer!");
+		fizzBuzz(maxValue);
+	}  else {
+		alert("FizzBuzz can only accept whole numbers. Please reload the page and enter an integer.");
+	}
 }
-console.log(maxValue);
 
-fizzBuzz(maxValue);
 
 function fizzBuzz(max) {
 for (i = 1; i <= max; i++) {
@@ -26,6 +40,5 @@ for (i = 1; i <= max; i++) {
 	}
   }
 }
-
 
 });
